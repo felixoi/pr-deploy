a0e48df8166cae905b7d3a9d78c8afdb75dd3aa5
Felix Kleinekathoefer <git@felixoi.com>
2020-10-13 15:43:39 +0200

    Cmon..

--- scripts/pr-deploy.sh
+++ scripts/pr-deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 cd preview || exit 1
-git config user.name "Deploy"
+git config user.name "felixoi"
 git config user.email "felixoi@users.noreply.github.com"
 git remote rm origin
-git remote add origin "https://$PA_TOKEN@github.com/felixoi/pr-preview"
+git remote set-url origin "https://felixoi:$PA_TOKEN@github.com/felixoi/pr-preview"
 
 if [ -d "$PULL_NUMBER" ]; then
   echo "Updating preview for pull request #$PULL_NUMBER..."
   rm -r ./"$PULL_NUMBER"
-  rsync -av --progress ./../deploy ./"$PULL_NUMBER" --exclude .git
+  rsync -av --progress ./../deploy ./"$PULL_NUMBER" --exclude={'.git','.github','scripts'}
 else
   echo "Creating preview for pull request #$PULL_NUMBER..."
-  rsync -av --progress ./../deploy ./"$PULL_NUMBER" --exclude .git
+  rsync -av --progress ./../deploy ./"$PULL_NUMBER" --exclude={'.git','.github','scripts'}
 fi
 
 git add -A
