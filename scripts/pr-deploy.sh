c9f923f14aca0ea6a34c35c9d850eeb3f080d80c
Felix Kleinekathoefer <git@felixoi.com>
2020-10-13 13:58:45 +0200

    Run on pull request

--- scripts/pr-deploy.sh
+++ scripts/pr-deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-for entry in "$PWD"/*
-do
-  echo "$entry"
-done
+echo ${$GITHUB_REF | cut -d '/' -f 3}
 
-cd preview || exit 1
-git status
-
-# deployment logic goes here
-sleep 10
 # instead we randomly succeed or fail the deployment
 exit $(( $RANDOM % 10 >= 5 ))
