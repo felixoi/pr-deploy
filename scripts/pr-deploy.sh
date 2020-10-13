5bcfda732a05e03646bcb3b5c868b39d658b3e0f
Felix Kleinekathoefer <git@felixoi.com>
2020-10-13 15:38:41 +0200

    Maybe this

--- scripts/pr-deploy.sh
+++ scripts/pr-deploy.sh
@@ -4,20 +4,20 @@ cd preview || exit 1
 git config user.name "Deploy"
 git config user.email "felixoi@users.noreply.github.com"
 git remote rm origin
-git remote add origin "https://felixoi:$PA_TOKEN@github.com/felixoi/pr-preview"
+git remote add origin "https://$PA_TOKEN@github.com/felixoi/pr-preview"
 
 if [ -d "$PULL_NUMBER" ]; then
   echo "Updating preview for pull request #$PULL_NUMBER..."
   rm -r ./"$PULL_NUMBER"
-  cp -r ./../deploy ./"$PULL_NUMBER"
+  rsync -av --progress ./../deploy ./"$PULL_NUMBER" --exclude .git
 else
   echo "Creating preview for pull request #$PULL_NUMBER..."
-  cp -r ./../deploy ./"$PULL_NUMBER"
+  rsync -av --progress ./../deploy ./"$PULL_NUMBER" --exclude .git
 fi
 
 git add -A
-git commit -q -m "Deploy $(date)"
-git push -q origin gh-pages
+git commit -m "Deploy $(date)"
+git push origin gh-pages
 
 # instead we randomly succeed or fail the deployment
 exit $(( $RANDOM % 10 >= 5 ))
